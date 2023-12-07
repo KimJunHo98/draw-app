7e08f6c2eb1aa8f7bb21815408ac2b665db43054
KimJunHo98 <dydgnscc6845@gmail.com>
2023-12-07 21:52:17 +0900

    231207 선 색상& 배경 색상 변경기능, 지우기 기능, 리셋 기능, 지우기&그리기 토글 기능 적용

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 const canvas = document.querySelector(".canvas");
+const canvasArea = document.querySelector(".canvas_area");
 const stroke = document.querySelector(".stroke");
+const color = document.querySelector(".color");
+const bgc = document.querySelector(".bgc");
+const eraser = document.querySelector(".eraser");
+const eraserCursor = document.querySelector(".eraser_cursor");
+const eraserSize = document.querySelector(".eraser_size");
+const draw = document.querySelector(".draw");
+const reset = document.querySelector(".reset");
 const options = document.querySelector(".options");
-const step = document.querySelector(".step");
+const optionBtn = document.querySelector(".option_btn");
+const stroke_step = document.querySelector(".stroke_step");
+const eraserSizeStep = document.querySelector(".eraserSize_step");
 const board = canvas.getContext("2d");
 
-const colors = ["#ff3838", "#ffb8b8", "#c56cf0", "#ff9f1a", "#fff200", "#32ff7e", "#7efff5", "#18dcff", "#7d5fff"];
-const color = colors[Math.floor(Math.random() * colors.length)];
-
 let isPainting = false;
+let isDelete = false;
 
-canvas.width = 800;
-canvas.height = 800;
-
+canvas.width = canvasArea.clientWidth;
+canvas.height = canvasArea.clientHeight;
 board.lineWidth = stroke.value;
 
-const handleMouseMove = (e) => {
+// 지우개 커서 움직임 함수
+function handleMouseMove(e) {
     const x = e.offsetX;
     const y = e.offsetY;
 
@@ -26,30 +34,128 @@ const handleMouseMove = (e) => {
     }
 
     board.moveTo(x, y);
-};
-const handlestrokeChange = (e) => {
-    board.lineWidth = e.target.value;
-    step.innerText = e.target.value;
 }
+
+// 선 굴기 변경 함수
+function handlestrokeChange(e) {
+    board.lineWidth = e.target.value;
+    stroke_step.innerText = e.target.value;
+}
+
 const handleOptionsClick = () => {
     options.classList.toggle("active");
+};
+
+// 색상 변경 함수
+function handleColorChange(e) {
+    const { value } = e.target;
+
+    board.strokeStyle = value;
+    board.fillStyle = value;
+}
+function handleBackgroundColorChange(e) {
+    board.fillStyle = e.target.value;
+    board.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// 지우기 함수
+function handleEraserClick(e) {
+    isDelete = true;
+
+    if (isDelete) {
+        board.lineWidth = e.target.value;
+        board.strokeStyle = bgc.value;
+        board.fillStyle = bgc.value;
+
+        stroke.disabled = true;
+
+        eraserSize.disabled = false;
+        eraserCursor.style.width = stroke.value;
+        eraserCursor.style.height = stroke.value;
+
+        eraserCursor.classList.add("on");
+        canvasArea.classList.add("eraser");
+    }
+}
+function handleMouseMoveWithEraser(e) {
+    const x = e.clientX - eraserSize.value / 2;
+    const y = e.clientY - eraserSize.value / 2;
+
+    eraserCursor.style.left = x + "px";
+    eraserCursor.style.top = y + "px";
+}
+function handleEraserSizeChange(e) {
+    board.lineWidth = e.target.value;
+    eraserCursor.style.width = e.target.value + "px";
+    eraserCursor.style.height = e.target.value + "px";
+    eraserSizeStep.innerText = e.target.value;
+}
+
+// 그리기 함수
+function handleDrawclick() {
+    isDelete = false;
+
+    if (!isDelete) {
+        board.lineWidth = stroke.value;
+        board.strokeStyle = color.value;
+        board.fillStyle = color.value;
+
+        stroke.disabled = false;
+
+        eraserSize.value = 3;
+        eraserSize.disabled = true;
+        eraserCursor.style.width = 3 + "px";
+        eraserCursor.style.height = 3 + "px";
+        eraserSizeStep.innerText = eraserSize.value;
+
+        eraserCursor.classList.remove("on");
+        canvasArea.classList.remove("eraser");
+    }
+}
+
+// 리셋
+function handleResetClick() {
+    board.fillStyle = "#fff";
+    board.fillRect(0, 0, canvas.width, canvas.height);
+
+    stroke.disabled = false;
+    stroke.value = 3;
+    stroke_step.innerText = 3;
+
+    eraserSizeStep.innerText = 3;
+    eraserSize.value = 3;
+    eraserSize.disabled = true;
+    eraserCursor.style.width = 3 + "px";
+    eraserCursor.style.height = 3 + "px";
+
+    color.value = "#000000";
+    bgc.value = "#ffffff";
+
+    handleDrawclick();
 }
 
 // 마우스를 누르지 않을 때(false) 그리기 종료,  마우스를 누르고 있을 때(true)만 그리기
-const canclePainting = () => {
+function canclePainting() {
     isPainting = false;
 
     board.beginPath();
-}; 
-const startPainting = () => {
+}
+
+function startPainting() {
     isPainting = true;
-};
+}
 
 canvas.addEventListener("pointermove", handleMouseMove);
 canvas.addEventListener("pointerup", canclePainting);
 canvas.addEventListener("pointerdown", startPainting);
 canvas.addEventListener("pointerleave", canclePainting);
+canvasArea.addEventListener("pointermove", handleMouseMoveWithEraser);
 
 stroke.addEventListener("change", handlestrokeChange);
-
-options.addEventListener("click", handleOptionsClick);
+color.addEventListener("change", handleColorChange);
+bgc.addEventListener("change", handleBackgroundColorChange);
+eraser.addEventListener("click", handleEraserClick);
+eraserSize.addEventListener("change", handleEraserSizeChange);
+draw.addEventListener("click", handleDrawclick);
+reset.addEventListener("click", handleResetClick);
+optionBtn.addEventListener("click", handleOptionsClick);
